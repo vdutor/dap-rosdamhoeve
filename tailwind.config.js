c19cdd441b8c8afe662fd6cc153a9c354616120d
Vincent Dutordoir <dutordoirv@gmail.com>
2022-04-11 22:44:38 +0100

    Work on body

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
       xl: '1440px',
     },
     extend: {
+      colors: {
+        blueGreen: '#0E6F70',
+        blue: '#75ABB0',
+        lightBlue: '#8FBAAC',
+        green: '#B9C422',
+        lightGreen: '#D4D991'
+      },
       backgroundImage: (theme) => ({
         dog: "url('../img/bg-image.jpg')",
       }),
